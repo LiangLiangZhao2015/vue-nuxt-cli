a731eceffe883812610935e4651a7a6c93c68c85
Allasm98 <zllas55@aliyun.com>
2018-01-26 17:29:07 +0800

    :fire: 修复

--- api.config.js
+++ api.config.js
@@ -1,5 +1,5 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-  baseUrl: isProdMode ? 'https://www.xuechenlei.com/' : 'http://localhost:9999/'
+    baseUrl: isProdMode ? 'https://www.zhaoliangliang.com/' : 'http://localhost:9999/'
 }
